59781f1a3ef3c563ebc328748055e98f131fa4f9
racha <rachaafaf15@gmail.com>
2025-01-31 17:56:05 +0100

    path error

--- functions/frontend/src/components/Auth/Login.js
+++ functions/frontend/src/components/Auth/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
-import "./Form.css";
+import "../../styles/Form.css";
 
 const Login = () => {
   const { login } = useAuth();
