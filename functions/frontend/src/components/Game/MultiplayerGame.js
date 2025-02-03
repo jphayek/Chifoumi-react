440a4c6f101a0d2c4ddb32e587467e06830f64f0
jphayek <jphayek1@gmail.com>
2025-02-03 20:50:56 +0100

    BugFix MultiGames

--- functions/frontend/src/components/Game/MultiplayerGame.js
+++ functions/frontend/src/components/Game/MultiplayerGame.js
@@ -30,7 +30,7 @@ function MultiplayerGame() {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`
       },
     })
       .then((res) => res.json())
